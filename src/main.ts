a91afeca91f66c6ac04379661351dd001f907c8f
diegowmmuller <diegomaglia96@gmail.com>
2025-07-10 11:24:32 -0300

    feat: Configuração do prisma

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableShutdownHooks();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Remove propriedades que não têm decorators no DTO
